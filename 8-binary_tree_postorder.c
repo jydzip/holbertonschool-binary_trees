c1acbf29748449628b9fa861baf732d9b4d64a1f
jydzip <dckjeremy.zip@gmail.com>
2024-04-08 15:09:34 +0200

    Add functions for traverse order in binary tree

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - Binary tree using post-order traversal
+ * @tree: Binary Tree
+ * @func: Function
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+	func(tree->n);
+}
