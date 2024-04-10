6b4710a6877c5c3b069988202e57b241e77c0cc6
jydzip <dckjeremy.zip@gmail.com>
2024-04-10 10:21:57 +0200

    Fix calculate height function for a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,14 +9,15 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t count = 0;
+	size_t height_l = 0, height_r = 0;
 
-	if (tree == NULL)
-		return (count);
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
 
-	if (tree->left != NULL)
-		count++;
-	if (tree->right != NULL)
-		count++;
-	return (count);
+	height_l = binary_tree_height(tree->left);
+	height_r = binary_tree_height(tree->right);
+
+	if (height_l > height_r)
+		return (height_l + 1);
+	return (height_r + 1);
 }
